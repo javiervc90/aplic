796c2264a5b4bc574f89d1669d392af08ab95ffe
Javier Velazquez <javier.velazquez@crowdint.com>
2011-11-10 15:59:48 -0600

    creating a new repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aplic::Application.config.secret_token = '2a16e4058be02b1f7b2487061b1849cc5bbe7e0ec2ffe8a562a0a255e2b48e2f3657ea43a898a93851b2785ad2c24b97b688dc686f8806e049cd0bb90d4c3e88'

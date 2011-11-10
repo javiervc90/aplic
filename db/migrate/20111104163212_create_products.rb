50df9972a7815b529443e48628a4bcd071367cc7
javier velazquez <javier.velazquez@crowdint.com>
2011-11-10 16:43:32 -0600

    modificando ficha registro

--- db/migrate/20111104163212_create_products.rb
+++ db/migrate/20111104163212_create_products.rb
@@ -2,6 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def self.up
 
     create_table :products do |t|
+      t.int :cuenta
       t.string :title
       t.string :description
       t.string :image_url
